63baac34b4154775885c23c7eda57fd4eccde673
Akanksha Verma <22906.akanksha@gmail.com>
2021-11-05 15:02:36 +0530

    Add files via upload

---
+++ chatbot.py
@@ -0,0 +1,214 @@
+import os
+import aiml
+import tkinter
+from tkinter import *
+
+BRAIN_FILE = "brain.dump"
+
+k = aiml.Kernel()
+
+# To increase the startup speed of the bot it is
+# possible to save the parsed aiml files as a
+# dump. This code checks if a dump exists and
+# otherwise loads the aiml from the xml files
+# and saves the brain dump.
+if os.path.exists(BRAIN_FILE):
+    print("Loading from brain file: " + BRAIN_FILE)
+    k.loadBrain(BRAIN_FILE)
+else:
+    print("Parsing aiml files")
+    k.bootstrap(learnFiles="std-startup.aiml", commands="load aiml b")
+    print("Saving brain file: " + BRAIN_FILE)
+    k.saveBrain(BRAIN_FILE)
+
+bot_name = "Cobot"
+
+# Endless loop which passes the input to the bot and prints
+# its response
+#while True:
+    #print("Let's chat! (type 'quit' to exit)")
+    #input_text = input(" Human > ")
+    #if input_text == "quit":
+        #break
+    #response = k.respond(input_text)
+    #print(" Corona Bot  >  "+response)
+
+# GUI part
+from tkinter import *
+
+# GUI class for the chat
+class GUI:
+    # constructor method
+    def __init__(self):
+        # chat window which is currently hidden
+        self.Window = Tk()
+        self.Window.withdraw()
+
+        # login window
+        self.login = Toplevel()
+        # set the title
+        self.login.title("Login")
+        self.login.resizable(width=False,
+                             height=False)
+        self.login.configure(width=400,
+                             height=300)
+        # create a Label
+        self.pls = Label(self.login,
+                         text="Please login to continue",
+                         justify=CENTER,
+                         font="Helvetica 14 bold")
+
+        self.pls.place(relheight=0.15,
+                       relx=0.2,
+                       rely=0.07)
+        # create a Label
+        self.labelName = Label(self.login,
+                               text="Name: ",
+                               font="Helvetica 12")
+
+        self.labelName.place(relheight=0.2,
+                             relx=0.1,
+                             rely=0.2)
+
+        # create a entry box for
+        # tyoing the message
+        self.entryName = Entry(self.login,
+                               font="Helvetica 14")
+
+        self.entryName.place(relwidth=0.4,
+                             relheight=0.12,
+                             relx=0.35,
+                             rely=0.2)
+
+        # set the focus of the cursor
+        self.entryName.focus()
+
+        # create a Continue Button
+        # along with action
+        self.go = Button(self.login,
+                         text="CONTINUE",
+                         font="Helvetica 14 bold",
+                         command=lambda: self.goAhead(self.entryName.get()))
+
+        self.go.place(relx=0.4,
+                      rely=0.55)
+        self.Window.mainloop()
+
+    def goAhead(self, name):
+            self.login.destroy()
+            self.layout(name)
+
+    # The main layout of the chat
+
+    def layout(self, name):
+        self.name = name
+        # to show chat window
+        self.Window.deiconify()
+        self.Window.title("CHATROOM")
+        self.Window.resizable(width=False,
+                              height=False)
+        self.Window.configure(width=470,
+                              height=550,
+                              bg="#17202A")
+        self.labelHead = Label(self.Window,
+                               bg="#17202A",
+                               fg="#EAECEE",
+                               text=self.name,
+                               font="Helvetica 13 bold",
+                               pady=5)
+
+        self.labelHead.place(relwidth=1)
+        self.line = Label(self.Window,
+                          width=450,
+                          bg="#ABB2B9")
+
+        self.line.place(relwidth=1,
+                        rely=0.07,
+                        relheight=0.012)
+
+        self.textCons = Text(self.Window,
+                             width=20,
+                             height=2,
+                             bg="#17202A",
+                             fg="#EAECEE",
+                             font="Helvetica 14",
+                             padx=5,
+                             pady=5)
+
+        self.textCons.place(relheight=0.745,
+                            relwidth=1,
+                            rely=0.08)
+
+        self.labelBottom = Label(self.Window,
+                                 bg="#ABB2B9",
+                                 height=80)
+
+        self.labelBottom.place(relwidth=1,
+                               rely=0.825)
+
+        self.entryMsg = Entry(self.labelBottom,
+                              bg="#2C3E50",
+                              fg="#EAECEE",
+                              font="Helvetica 13")
+
+        # place the given widget
+        # into the gui window
+        self.entryMsg.place(relwidth=0.74,
+                            relheight=0.06,
+                            rely=0.008,
+                            relx=0.011)
+
+        self.entryMsg.focus()
+
+        # create a Send Button
+        self.buttonMsg = Button(self.labelBottom,
+                                text="Send",
+                                font="Helvetica 10 bold",
+                                width=20,
+                                bg="#ABB2B9",
+                                command=lambda: self._on_enter_pressed(None))
+
+        self.buttonMsg.place(relx=0.77,
+                             rely=0.008,
+                             relheight=0.06,
+                             relwidth=0.22)
+
+        self.textCons.config(cursor="arrow")
+
+        # create a scroll bar
+        scrollbar = Scrollbar(self.textCons)
+
+        # place the scroll bar
+        # into the gui window
+        scrollbar.place(relheight=1,
+                        relx=0.974)
+
+        scrollbar.config(command=self.textCons.yview)
+
+        self.textCons.config(state=DISABLED)
+
+    def _on_enter_pressed(self, event):
+        msg = self.entryMsg.get()
+        self._insert_message(msg, "You")
+
+    def _insert_message(self, msg, sender):
+        if not msg:
+            return
+
+        self.entryMsg.delete(0, END)
+        msg = f"{sender}: {msg}\n\n"
+        self.textCons.configure(cursor="arrow", state=NORMAL)
+        self.textCons.insert(END, msg)
+        self.textCons.configure(state=DISABLED)
+        #msg1 = input(" Human > ")
+
+        response = k.respond(msg)
+        msg2 = f"{bot_name}: {response}\n\n"
+        self.textCons.configure(cursor="arrow", state=NORMAL)
+        self.textCons.insert(END, msg2)
+        self.textCons.configure(state=DISABLED)
+        self.textCons.see(END)
+
+    # create a GUI class object
+g = GUI()
+
